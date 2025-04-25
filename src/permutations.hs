029f2fb417dfa04075d2495d81f3f2d05a040db9
@HM65 <humanist1965@gmail.com>
2025-04-25 16:57:32 +0000

    update

--- src/permutations.hs
+++ src/permutations.hs
@@ -3,6 +3,11 @@
 {-# HLINT ignore "Use camelCase" #-}
 
 import Data.List (permutations)
+import System.CPUTime
+import Control.Monad (when)
+import Data.Time.Clock (diffUTCTime, getCurrentTime) -- Import for a slightly different time
+
+
 
 
 type StartPos = Int
@@ -50,3 +55,67 @@ genPerm start numItems maxPos partialList resList =
                     genPerm nextNum nextSize maxPos partialList2 res
             ) resList optionsList
         in permsList
+
+
+-- Optimized genPerm function using a Set to track used elements
+genPermOptimized :: StartPos -> NumItems -> MaxPos -> PermList -> [PermList] -> [PermList]
+genPermOptimized _ 0 _ partialList resList = partialList : resList
+genPermOptimized start numItems maxPos partialList resList =
+  let
+    -- Generate options more efficiently using a list comprehension and set difference
+    optionsList = [n | n <- [0 .. maxPos - 1], n `notElem` partialList]
+    in
+    foldl
+      (\acc option ->
+         genPermOptimized
+           start
+           (numItems - 1)
+           maxPos
+           (option : partialList)
+           acc
+      )
+      resList
+      optionsList
+
+-- Wrapper function to start with an empty accumulator
+perms1 :: NumItems ->  [PermList]
+perms1 numItems  = genPermOptimized 0 numItems numItems [] []
+
+
+perms2 :: NumItems ->  [PermList]
+perms2 numItems  = genPerm 0 numItems numItems [] []
+
+
+
+
+
+-- Modified timedAction to take the action as a parameter
+timedAction :: String -> IO a -> IO a
+timedAction description action = do
+  start_cpu <- getCPUTime
+  start_real <- getCurrentTime -- Use getCurrentTime for wall clock time
+  result <- action
+  end_cpu <- getCPUTime
+  end_real <- getCurrentTime
+  let duration_cpu = (fromIntegral (end_cpu - start_cpu)) / (10^12) :: Double -- Convert to seconds
+      duration_real = realToFrac (diffUTCTime end_real start_real) :: Double
+  -- putStrLn $ description ++ " (CPU): " ++ show duration_cpu ++ " seconds"
+  putStrLn $ description ++ " (Real): " ++ show duration_real ++ " seconds"
+  return result
+
+out :: [PermList] -> IO ()
+out res = do
+  putStrLn $ "Number perms = " ++ show (length res)
+
+
+main :: IO ()
+main = do
+  timedAction "perms1 8" (out $ perms1 8)
+  timedAction "perms2 8" (out $ perms2 8)
+  timedAction "perms1 9" (out $ perms1 9)
+  timedAction "perms2 9" (out $ perms2 9)
+  timedAction "perms1 10" (out $ perms1 10)
+  timedAction "perms2 10" (out $ perms2 10)
+  timedAction "permutatiions" (out $ permutations [0..9])
+  
+  
