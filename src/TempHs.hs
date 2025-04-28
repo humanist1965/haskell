750802fc5204e95aab8896d5559cdcc9827ef808
@HM65 <humanist1965@gmail.com>
2025-04-28 19:51:12 +0000

    update

--- src/TempHs.hs
+++ src/TempHs.hs
@@ -1,9 +1,10 @@
 
     module TempHs(main) where
-    import Factorials (combinations_count)
+    import Factorials (combinations_count, factorial)
     main :: IO ()
     main = do 
         putStrLn ""
         putStrLn "Number of ways to place 8 queens on an 8x8 board:"
         print $ combinations_count 64 8
+        print $ factorial 15
 
